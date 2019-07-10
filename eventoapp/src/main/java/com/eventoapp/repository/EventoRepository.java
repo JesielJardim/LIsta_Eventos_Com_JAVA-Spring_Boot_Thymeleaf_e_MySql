81df7e56b080af8d649fa6d37359b0b2625d3b4f
Jesiel <44484523+JesielJardim@users.noreply.github.com>
2019-07-10 10:00:16 -0300

    Update EventoRepository.java

--- eventoapp/src/main/java/com/eventoapp/repository/EventoRepository.java
+++ eventoapp/src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.eventoapp.models.Evento;
 
-public interface EventoRepository extends CrudRepository<Evento, String> { //ja vai utilizar metodos prontos com essa extensão
+public interface EventoRepository extends CrudRepository<Evento, String> { 
 
-	Evento findByCodigo(long codigo); //criado evento para pesquisarpor codigo
+	Evento findByCodigo(long codigo); 
 }
